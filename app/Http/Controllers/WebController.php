5bfbc071d60f226975af58636436eea342223208
TheOnlySmartBoy <o2jose43@gmail.com>
2019-09-26 11:41:56 +0300

    Added the contact contact logic

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -2,19 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactMailer;
 use Illuminate\Http\Request;
 
-class WebController extends Controller
-{
-    public function about(){
+class WebController extends Controller {
+
+    public function about() {
         return view('front.about');
     }
-    
-    public function contact(Request $request){
-        if($request->isMethod('post')){
-            $data = $request->all();
-        }else{
+
+    public function contact(Request $request) {
+        if ($request->isMethod('post')) {
+
+            $data = $request->validate([
+                'c_name' => 'required',
+                'c_email' => 'required|email',
+                'c_message' => 'required',
+            ]);
+            Mail::to('info@savemikiundu.org')->send(new ContactMailer());
+        } else {
             return view('front.contact');
         }
     }
+
 }
--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -1,53 +1,53 @@
 @extends('layouts.frontlayout.front_design')
 @section('content')
-    <div class="page-header">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <h1>Contact</h1>
-                </div><!-- .col -->
-            </div><!-- .row -->
-        </div><!-- .container -->
-    </div><!-- .page-header -->
-    <div class="contact-page-wrap">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-lg-5">
-                    <div class="entry-content">
-                        <h2>Get In touch with us</h2>
-                        <p>We are always open to suggestions and ideas feel free to talk to us if you have any ideas that would help us make our community better.<br>
+<div class="page-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Contact</h1>
+            </div><!-- .col -->
+        </div><!-- .row -->
+    </div><!-- .container -->
+</div><!-- .page-header -->
+<div class="contact-page-wrap">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-5">
+                <div class="entry-content">
+                    <h2>Get In touch with us</h2>
+                    <p>We are always open to suggestions and ideas feel free to talk to us if you have any ideas that would help us make our community better.<br>
                         You can visit us in our office, give us a call or better still leave us a note by filling the form bellow</p>
-                        <ul class="contact-social d-flex flex-wrap align-items-center">
-                            <li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
-                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-                            <li><a href="#"><i class="fa fa-behance"></i></a></li>
-                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                        </ul>
-                        <ul class="contact-info p-0">
-                            <li><i class="fa fa-phone"></i><span>+45 677 8993000 223</span></li>
-                            <li><i class="fa fa-envelope"></i><span>office@template.com</span></li>
-                            <li><i class="fa fa-map-marker"></i><span>Main Str. no 45-46, b3, 56832, Los Angeles, CA</span></li>
-                        </ul>
-                    </div>
-                </div><!-- .col -->
-                <div class="col-12 col-lg-7">
-                    <form class="contact-form" method="post" action="{{url('contact')}}">@csrf
-                        <input type="text" placeholder="Name" required>
-                        <input type="email" placeholder="Email" required>
-                        <textarea rows="15" cols="6" placeholder="Messages" required></textarea>
-                        <span>
-                            <input class="btn gradient-bg" type="submit" value="Contact us">
-                        </span>
-                    </form><!-- .contact-form -->
-                </div><!-- .col -->
-                <div class="col-12">
-                    <div class="contact-gmap">
-                        <iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=usa&key=AIzaSyC2LvnNLzWxHgFm_XfpFG9wHUuyEj6rXSs" allowfullscreen></iframe>
-                    </div>
+                    <ul class="contact-social d-flex flex-wrap align-items-center">
+                        <li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
+                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                        <li><a href="#"><i class="fa fa-behance"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                    </ul>
+                    <ul class="contact-info p-0">
+                        <li><i class="fa fa-phone"></i><span>+45 677 8993000 223</span></li>
+                        <li><i class="fa fa-envelope"></i><span>office@template.com</span></li>
+                        <li><i class="fa fa-map-marker"></i><span>Main Str. no 45-46, b3, 56832, Los Angeles, CA</span></li>
+                    </ul>
                 </div>
-            </div><!-- .row -->
-        </div><!-- .container -->
-    </div>
-    @endsection
+            </div><!-- .col -->
+            <div class="col-12 col-lg-7">
+                <form class="contact-form" method="post" action="{{url('contact')}}">@csrf
+                    <input type="text" name="c_name" placeholder="Name" required>
+                    <input type="email" name="c_email" placeholder="Email" required>
+                    <textarea rows="15" cols="6" name="c_message" placeholder="Messages" required></textarea>
+                    <span>
+                        <input class="btn gradient-bg" type="submit" value="Contact us">
+                    </span>
+                </form><!-- .contact-form -->
+            </div><!-- .col -->
+            <div class="col-12">
+                <div class="contact-gmap">
+                    <iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=usa&key=AIzaSyC2LvnNLzWxHgFm_XfpFG9wHUuyEj6rXSs" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div><!-- .row -->
+    </div><!-- .container -->
+</div>
+@endsection
